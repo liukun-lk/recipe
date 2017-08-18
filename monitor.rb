68b44b2a611dc1235a4fb4c91a7df0a6e5323b75
feelmix <feelmix@feelmixdeMBP.lan>
2017-08-18 23:52:43 +0800

    example but i forget why use it :(

--- monitor.rb
+++ monitor.rb
@@ -55,26 +55,27 @@ require 'thread'
 #   end
 # end
 # 5. -------------------
-class Counter
-  attr_reader :number, :q
-  def initialize
-    @number = 0
-    @q = Queue.new
-  end
+# maybe has some problems
+# class Counter
+#   attr_reader :number, :q
+#   def initialize
+#     @number = 0
+#     @q = Queue.new
+#   end
 
-  def start
-    (1..20_000).each do
-      @q.push(1)
-    end
-  end
+#   def start
+#     (1..20_000).each do
+#       @q.push(1)
+#     end
+#   end
 
-  def plus
-    while line = q.pop
-      @number += 1
-      p @number
-    end
-  end
-end
+#   def plus
+#     while line = q.pop
+#       @number += 1
+#       p @number
+#     end
+#   end
+# end
 
 
 # c = Counter.new
@@ -82,13 +83,13 @@ end
 # t2 = Thread.new { 10_000.times { c.plus } }
 # t1.join
 # t2.join
-c = Counter.new
-c.start
-t1 = Thread.new { 10_000.times { c.plus } }
-t2 = Thread.new { 10_000.times { c.plus } }
-t1.join
-t2.join
-puts c.number
+# c = Counter.new
+# c.start
+# t1 = Thread.new { 10_000.times { c.plus } }
+# t2 = Thread.new { 10_000.times { c.plus } }
+# t1.join
+# t2.join
+# puts c.number
 
 # q = Queue.new
 
@@ -103,3 +104,55 @@ puts c.number
 # while (u = gets)
 #   q.push(u)
 # end
+
+# def counters_with_mutex
+#   mutex = Mutex.new
+#   counters = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+#   5.times.map do
+#     Thread.new do
+#       100000.times do
+#         mutex.synchronize do
+#           counters.map! { |counter| counter + 1 }
+#         end
+#       end
+#     end
+#   end.each(&:join)
+
+#   counters.inspect
+# end
+
+# def counters_without_mutex
+#   counters = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+#   5.times.map do
+#     Thread.new do
+#       100000.times do
+#         counters.map! { |counter| counter + 1 }
+#       end
+#     end
+#   end.each(&:join)
+
+#   counters.inspect
+# end
+
+# puts counters_with_mutex
+# # => [500000, 500000, 500000, 500000, 500000, 500000, 500000, 500000, 500000, 500000]
+
+# puts counters_without_mutex
+# # => [500000, 447205, 500000, 500000, 500000, 500000, 203656, 500000, 500000, 500000]
+
+mutex = Mutex.new
+flags = [false, false, false, false, false, false, false, false, false, false]
+
+threads = 50.times.map do
+  Thread.new do
+    100000.times do
+      puts flags.to_s
+      mutex.synchronize do
+        flags.map! { |f| !f }
+      end
+    end
+  end
+end
+threads.each(&:join)
