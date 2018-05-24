70b84f4df1c3f8fbd631ea81f7e719af24e075fe
liukun-lk <f221mix@gmail.com>
2018-05-24 22:45:40 +0800

    update use case code

--- use_ensure_for_exception.rb
+++ use_ensure_for_exception.rb
@@ -1,10 +1,22 @@
 # Sometimes you will look this
+# http://benjamintan.io/blog/2015/03/28/ruby-block-patterns-and-how-to-implement-file-open/
+file = File.open(file_name, 'w') do
+  # do something
+  # if raise exception?
+  # the file can close itself? Yes!
+end
+
+# and if you do like that
+f = File.open('Leo Tolstoy - War and Peace.txt', 'w')
+f << "Well, Prince, so Genoa and Lucca"
+f << " are now just family estates of the Buonapartes."
+f.close
+
+# when write file raise exception, file can't be closed correct.
 begin
-  file = File.open(file_name, 'w') do
-    # do something
-    # if raise exception?
-    # the file can close itself?
-  end
+  f = File.open('Leo Tolstoy - War and Peace.txt', 'w')
+  f << "Well, Prince, so Genoa and Lucca"
+  f << " are now just family estates of the Buonapartes."
 ensure
-  file.close if file
+  f&.close
 end
